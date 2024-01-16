a7d269a7f39d14b023d5be5ecd17fd3fff6b045c
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-01-17 00:49:13 +0100

    Implemented target web service

---
+++ MonitoringAPI/Controllers/MonitoringController.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using MonitoringAPI.Models;
+using Newtonsoft.Json;
+
+namespace MonitoringAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MonitoringController : ControllerBase
+    {
+        public MonitoringController() { }
+
+        [HttpPost("image")]
+        public async Task<IActionResult> ReceiveImage([FromForm] IFormCollection collection)
+        {
+            DateTime time;
+
+            using (Stream stream = collection.Files[0].OpenReadStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string json = await reader.ReadToEndAsync();
+                    Dictionary<string, string> info = JsonConvert.DeserializeObject<Dictionary<string, string>>(json)!;
+
+                    time = DateTime.Parse(info["time"]);
+                }
+            }
+
+            string outDirectory = string.Format("{0}/images/{1}/",
+                System.Reflection.Assembly.GetEntryAssembly().Location,
+                Request.HttpContext.Connection.RemoteIpAddress
+            );
+            string outFile = string.Format("{0}-{1}-{2}_{3}-{4}-{5}.jpg",
+                time.Year,
+                time.Month,
+                time.Day,
+                time.Hour,
+                time.Minute,
+                time.Second
+            );
+
+            Directory.CreateDirectory(outDirectory);
+
+            using (Stream stream = collection.Files[0].OpenReadStream())
+            {
+                using (FileStream fileStream = new FileStream(outDirectory + outFile, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    stream.CopyTo(fileStream);
+                }
+            }
+
+            return NoContent();
+        }
+    }
+}
