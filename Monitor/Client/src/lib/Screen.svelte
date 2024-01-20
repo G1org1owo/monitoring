feea1776813da0fc09cae13282beb3cfb876133f
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-01-20 17:14:59 +0100

    Add style for screen component

--- Monitor/Client/src/lib/Screen.svelte
+++ Monitor/Client/src/lib/Screen.svelte
@@ -6,14 +6,51 @@
 </script>
 
 <div class=image-container>
-    <img src={$image.imageUrl} alt="Loading...">
-    <p class=timestamp>{$image.timestamp}</p>
+    {#if $image.imageUrl}
+        <img src={$image.imageUrl} alt="Loading...">
+        <p class=timestamp>{$image.timestamp}</p>
+    {:else}
+        <p class=alt-text>Loading...</p>
+    {/if}
 </div>
 
 <style>
-    img {
+    .image-container {
+        position: relative;
         width: 480px;
         height: 272px;
+        text-align: center;
         border-radius: 10px;
+        background-color: black;
+    }
+
+    img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 480px;
+        height: 272px;
+        line-height: 200px;
+        text-align: center;
+    }
+
+    .timestamp {
+        position: absolute;
+        right: 10px;
+        bottom: 5px;
+        border-radius: 5px;
+        background: #1a1a1a8f;
+    }
+
+    .alt-text {
+        position: absolute;
+        width: 100px;
+        height:25px;
+        top: 50%;
+        left: 50%;
+        text-align: center;
+        line-height: 25px;
+        margin: -12.5px 0 0 -50px;
+        padding: 0;
     }
 </style>
