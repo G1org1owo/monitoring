cecb95e01bb42a524afee79e208b9dafc0b3b41a
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-01-22 00:35:31 +0100

    Move multiple screens to Dashboard.svelte

--- Monitor/Client/src/App.svelte
+++ Monitor/Client/src/App.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-  import Screen from "./lib/Screen.svelte";
-  import { createClientsStore } from "./lib/stores";
-
+  import Dashboard from './lib/Dashboard.svelte';
   const serverUrl = "http://127.0.0.1";
-  const clients = createClientsStore("")
+  
 </script>
 
 <main>
-  {#each $clients as client}
-    <Screen on:connectionLost={() => console.log("connection lost")}
-      src={serverUrl}
-      target={client}>
-    
-    </Screen>
-  {/each}
+  <Dashboard src={serverUrl}></Dashboard>
 </main>
 
 <style>
---
+++ Monitor/Client/src/lib/Dashboard.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Screen from "./Screen.svelte";
+    import { createClientsStore } from "./stores";
+    export let src;
+    
+    let size = 30;
+
+    /**
+     * @param {number} nClients
+     */
+    function getSizeFor(nClients) {        
+        if(nClients <= 1) {
+            return 100;
+        }
+
+        if(nClients <= 4) {
+            return 50;
+        }
+    }
+
+    const clients = createClientsStore("")
+
+    $: size = getSizeFor($clients.length);
+</script>
+
+<div class=container>
+{#each $clients as client}
+    <Screen on:connectionLost={() => console.log("connection lost")}
+        {src} target={client} {size}
+    >
+
+    </Screen>
+{/each}
+</div>
+
+<style>
+    .container {
+        display:flex;
+        flex-wrap: wrap;
+        grid-gap: 10px;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
